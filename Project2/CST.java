87471d31f14f35b0fbdb2a2799e0bf76ef6eb527
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-06-25 13:58:35 -0400

    started tree

---
+++ Project2/CST.java
@@ -0,0 +1,10 @@
+/*
+    CST - Concrete Syntax Tree
+    Tree representation of the Grammar(Rules of how the program should be written)
+    Will store and manipulate a stream of tokens in conjunction with our established grammer rules
+
+ */
+
+public class CST {
+    
+}
---
+++ Project2/Node.java
@@ -0,0 +1,9 @@
+/*
+    Node class for CST to hold values of each node in the tree
+    Parent - Contents - Children
+ */
+
+
+public class Node {
+    
+}

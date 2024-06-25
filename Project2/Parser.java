87c3aee536c1c2eed99aa6a75a21b72da9217068
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-06-25 12:35:32 -0400

    created parse methods

--- Project2/Parser.java
+++ Project2/Parser.java
@@ -10,10 +10,109 @@ public class Parser {
     // Private Instance Variables
     private ArrayList<Token> myTokens;
 
+    public void parseProgram() {
+
+    }
+
+    public void parseBlock() {
+
+    }
+
+    public void parseStatement() {
+
+    }
+
+    public void parseStatementList() {
+
+    }
+
+    public void parsePrint() {
+
+    }
+
+    public void parseAssignment() {
+
+    }
+
+    public void parseVarDecl() {
+
+    }
+
+    public void parseWhile() {
+
+    }
+
+    public void parseIf() {
+        
+    }
+
+    public void parseExpr() {
+
+    }
+
+    public void parseIntExpr() {
+
+    }
+
+    public void parseStringExpr() {
+
+    }
+
+    public void parseBooleanExpr() {
+
+    }
+
+    public void parseCharList() {
+
+    }
+
+    public void parseId() {
+
+    }
+
+    public void parseType() {
+
+    }
+
+    public void parseChar() {
+
+    }
+
+    public void parseSpace() {
+
+    }
+
+    public void parseDigit() {
+
+    }
+
+    public void parseBoolOp() {
+
+    }
+
+    public void parseBoolVal() {
+
+    }
+
+    public void parseIntOp() {
+        
+    }
+
+
+
+
     // Null Constructor
     public Parser() {
         myTokens = new ArrayList<Token>();
     }
+
+
+
+
+
+
+
+
 
     //! Method to read in tokens to parser
     public void tokenStream(ArrayList<Token> lexerTokens) {

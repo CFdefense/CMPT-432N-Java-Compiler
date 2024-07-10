2f663627519f348d04b3724d235df738edb0b373
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-07-10 14:43:42 -0400

    Begin Research for Project 4

---
+++ Project4/project4/GenerateMachineCode.java
@@ -0,0 +1,19 @@
+/*
+    Generate Machine Code
+    Takes an AST and Generates 6502 Machine Code
+
+*/
+
+package project4;
+
+// Create final max size of byte array
+
+public class GenerateMachineCode {
+    
+    // Private Instance Variables
+
+    // Constructor
+    public GenerateMachineCode() {
+        
+    }   
+}
